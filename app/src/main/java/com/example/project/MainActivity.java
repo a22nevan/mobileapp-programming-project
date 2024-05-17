aad0dbd0b8a9c0247a0ed7c518f133849534edf2
a22nevan <a22nevan@student.his.se>
2024-05-17 15:24:01 +0200

    -Temp commit

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView myWebView;
+
+    public void showInternalWebPage(){
+        myWebView.loadUrl("file:///android_asset/about.html");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // getMenuInflater().inflate(R.menu.menu_main);
+        return true;
     }
 
 }
